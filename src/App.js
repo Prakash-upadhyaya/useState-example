2ac88cab45cfae79cdf6a2f0dd51935c084d2554
Prakash-upadhyaya <96384323+Prakash-upadhyaya@users.noreply.github.com>
2022-07-23 17:32:00 +0530

    use state example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function Increment() {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Use State Example </h1>
+      <h1>{count} </h1>
+      <button onClick={Increment}>Click</button>
     </div>
   );
 }
